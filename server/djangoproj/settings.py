0e7289984ba93adc5d4cb6bdf8901c7c93b97d09
Indhu <indhumuppalla9@gmail.com>
2024-06-11 17:51:37 +0530

    new changes

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,14 +28,15 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-<<<<<<< HEAD
-ALLOWED_HOSTS = ['localhost',
-                 'https://indhumuppall-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS = ['https://indhumuppall-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
-=======
-ALLOWED_HOSTS=['localhost','https://indhumuppall-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS=['https://indhumuppall-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
->>>>>>> feef5af1c0b48c1e86c5ff46f25931d3ed3e30e3
+
+ALLOWED_HOSTS = [
+    'localhost',
+    'https://indhumuppall-8000.theiadockernext-0-labs-prod-theiak8s-4-'
+    'tor01.proxy.cognitiveclass.ai']
+CSRF_TRUSTED_ORIGINS = [
+    'https://indhumuppall-8000.theiadockernext-0-labs-prod-theiak8s-4-'
+    'tor01.proxy.cognitiveclass.ai']
+
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -100,7 +101,8 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'django.contrib.auth.password_validation.'
+        'UserAttributeSimilarityValidator',
     },
     {
         'NAME':
